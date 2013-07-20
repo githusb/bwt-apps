23657c84280d108bb89f2136712e5a0a71e0af8f
Daniel Greene <dgreene@dgreene-VirtualBox.(none)>
2013-07-20 21:18:46 +0100

    more notes...

--- bwt.py
+++ bwt.py
@@ -3,6 +3,7 @@ import sys
 #append lexographically smallest terminal charcter to input...
 in_str = sys.argv[1] + "$"
 
+#create substrings for sorting...
 sub_strs = [in_str[i:len(in_str)] + in_str[0:i] for i in range(len(in_str))]
 
 sub_strs.sort()
