41e2f0f99a908f73755faed2d7c79dadd119f875
Daniel Greene <dgreene@dgreene-VirtualBox.(none)>
2013-07-20 21:07:55 +0100

    add note

--- bwt.py
+++ bwt.py
@@ -1,5 +1,6 @@
 import sys
 
+#append lexographically smallest terminal charcter to input...
 in_str = sys.argv[1] + "$"
 
 sub_strs = [in_str[i:len(in_str)] + in_str[0:i] for i in range(len(in_str))]
